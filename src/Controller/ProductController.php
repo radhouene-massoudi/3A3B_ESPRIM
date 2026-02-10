763e56ef419f99592eafbdc65d39f6997524bf4b
massoudiradhouene <radhouane.massoudi@esprit.tn>
2026-02-10 09:51:00 +0100

    seance 10/2

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -66,6 +68,24 @@ $result=$repo->findById($r);
 return $this->render('product/detail.html.twig',[
     'd'=>$result[0]
 ]);
+
+    }
+    #[Route('/addp', name: 'addp')]
+    public function addProduct(ManagerRegistry $mr,Request $req)
+    {
+        $p=new Product();
+        $form=$this->createForm(ProductType::class,$p);
+        $form->handleRequest($req);
+        $em=$mr->getManager();
+        if($form->isSubmitted()){
+$em->persist($p);
+        $em->flush();
+        return $this->redirectToRoute('l');
+        }
+        return $this->render('product/addproduct.html.twig',[
+          'f'=>$form  
+        ]);
+
 
     }
 }
